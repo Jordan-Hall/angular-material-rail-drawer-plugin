80de442c5f21057168e90d74da9f263f90470425
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2021-11-20 00:43:11 +0100

    Update the project for a newer Angular version (#45)
    * In order to be able to use the raildrawer in newer Angular versions, the project was upgraded using the update guide for angular.
    * Use of yarn to minimise build time
    * Update the package versions information so that the package can be used.

--- projects/angular-material-rail-drawer/src/lib/animations.settings.ts
+++ projects/angular-material-rail-drawer/src/lib/animations.settings.ts
@@ -1,45 +1,52 @@
-import { group, query, animateChild, animate, style, AnimationStyleMetadata, AnimationGroupMetadata } from '@angular/animations';
+import {
+  group,
+  query,
+  animateChild,
+  animate,
+  style,
+  AnimationStyleMetadata,
+  AnimationGroupMetadata,
+} from '@angular/animations';
 import { miniConfig } from './default.config';
 
-export function sidebarClose(minWidth: string = miniConfig.defaultMinWidth): AnimationStyleMetadata {
+export function sidebarClose(
+  minWidth: string = miniConfig.defaultMinWidth
+): AnimationStyleMetadata {
   return style({
     width: minWidth,
     visibility: 'visible',
     transform: 'none',
-    overflow: 'hidden'
-  })
+    overflow: 'hidden',
+  });
 }
 
-
-
-export function sidebarOpen(maxWidth: string = miniConfig.defaultMaxWidth): AnimationStyleMetadata {
+export function sidebarOpen(
+  maxWidth: string = miniConfig.defaultMaxWidth
+): AnimationStyleMetadata {
   return style({
     width: maxWidth,
     visibility: 'visible',
-  })
+  });
 }
-
 
 export function sidebarAnimationOpenGroup(
   animationDuration: string = miniConfig.defaultDuration,
   maxWidth: string = miniConfig.defaultMaxWidth
-
 ): AnimationGroupMetadata {
   return group([
     query('@iconAnimation', animateChild(), { optional: true }),
     query('@labelAnimation', animateChild(), { optional: true }),
-    animate(`${animationDuration} ease-in-out`, sidebarOpen(maxWidth))
-  ])
+    animate(`${animationDuration} ease-in-out`, sidebarOpen(maxWidth)),
+  ]);
 }
 
 export function sidebarAnimationCloseGroup(
   animationDuration: string = miniConfig.defaultDuration,
   minWidth: string = miniConfig.defaultMinWidth
-
 ): AnimationGroupMetadata {
   return group([
     query('@iconAnimation', animateChild(), { optional: true }),
     query('@labelAnimation', animateChild(), { optional: true }),
-    animate(`${animationDuration} ease-in-out`, sidebarClose(minWidth))
-  ])
+    animate(`${animationDuration} ease-in-out`, sidebarClose(minWidth)),
+  ]);
 }
