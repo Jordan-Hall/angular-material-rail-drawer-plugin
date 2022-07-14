45b87b3057eed4aeaa34442347c1db8ca65a3703
Jordan Hall <jordan@libertyware.co.uk>
2022-07-14 10:54:58 +0100

    feat: move to nx for 14
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- projects/angular-material-rail-drawer/src/public-api.ts
+++ projects/angular-material-rail-drawer/src/public-api.ts
@@ -1,8 +1,6 @@
 /*
  * Public API Surface of angular-material-rail-drawer
  */
-
-export * from './lib/drawer-rail.module';
 export * from './lib/default.config';
 export * from './lib/animations.settings';
 export * from './lib/drawer-rail.directive';
